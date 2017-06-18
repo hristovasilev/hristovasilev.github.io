90ee67916fcfdbc13c498e3b155749f7d849d449
hristovasilev <christovasilevs@gmail.com>
2017-06-18 18:48:54 -0500

    6/18

--- FinalProject.js
+++ FinalProject.js
@@ -1,1 +1,21 @@
-let 
+let btn = document.getElementById("getFlightsButton");
+
+btn.onclick = function(e){
+    e.preventDefault();
+    let currentFlight = flights[0];
+
+    let ctn = document.getElementById("FlightCtn");
+
+    let message = ` 
+        <p> Date: ${currentFlight.flightDate} </p>
+        <p> Number of Seats: ${currentFlight.companyName} </p>
+        <p> From: ${currentFlight.from} </p>
+        <p> To: ${currentFlight.to} </p>
+        <p> Departure: ${currentFlight.departureTime} </p>
+        <p> Arrival: ${currentFlight.arrivalTime} </p>
+        <p> Max Amount: ${currentFlight.amount } </p>
+ 
+    `
+
+    FlightCtn.innerHTML = message;
+}
---
+++ SampleFlights.js
@@ -0,0 +1,53 @@
+var flights = [
+    {
+        'flightID'     : '1',
+        'companyName'  : '3',
+        'from'         : 'Chicago',
+        'flightDate'   : '06/23/2017',
+        'departureTime': '11:35 AM',
+        'to'           : 'Las Vegas',
+        'arrivalTime'  : '2:00 PM',
+        'amount'       : '$2000'
+
+    },
+    {
+        'flightID'     : '2',
+        'companyName'  : 'Delta',
+        'from'         : 'New York City',
+        'departureTime': '3:00 PM',
+        'to'           : 'Salt Lake City',
+        'arrivalTime'  : '5:30 PM',
+        'amount'       : '$80'
+
+    },
+    {
+        'flightID'     : '3',
+        'companyName'  : 'JetBlue',
+        'from'         : 'Seattle',
+        'departureTime': '8:15 AM',
+        'to'           : 'Boston',
+        'arrivalTime'  : '11:45 PM',
+        'amount'       : '$180'
+
+    },
+    {
+        'flightID'     : '4',
+        'companyName'  : 'Frontier',
+        'from'         : 'San Francisco',
+        'departureTime': '6:30 PM',
+        'to'           : 'Baton Rouge',
+        'arrivalTime'  : '9:45 PM',
+        'amount'       : '$175'
+
+    },
+    {
+        'flightID'     : '5',
+        'companyName'  : 'Spirit',
+        'from'         : 'St. Louis',
+        'departureTime': '1:00 PM',
+        'to'           : 'Pheonix',
+        'arrivalTime'  : '3:00 PM',
+        'amount'       : '$70'
+
+    }
+]
